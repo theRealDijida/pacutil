fa4a1d39010b86519814aff80217603546242432
dijida <4197759+theRealDijida@users.noreply.github.com>
2020-03-13 14:34:30 -0400

    updated swap to 6gb

--- pacglobal-mn.sh
+++ pacglobal-mn.sh
@@ -57,7 +57,7 @@ echo "###########################"
 echo "#   Setting up swapfile   #"
 echo "###########################"
 sudo swapoff -a
-sudo fallocate -l 4G /swapfile
+sudo fallocate -l 6G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
